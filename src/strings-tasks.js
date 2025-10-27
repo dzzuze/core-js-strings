052e87641f476b64ca9e5702a3f8f702093c29ca
dzzuze <notinsidik@gmail.com>
2025-10-27 16:25:34 +0300

    feat: implement extractEmails

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -491,8 +491,10 @@ function unbracketTag(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  const emailArr = str.split(';');
+  const trimEmail = emailArr.map((email) => email.trim());
+  return trimEmail;
 }
 
 /**
