f9b9da2447e41d3f3c58a890d0c3517fe8a7b89f
dzzuze <notinsidik@gmail.com>
2025-10-27 15:04:49 +0300

    feat: implement reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -397,8 +397,10 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const wordsArr = str.split(' ');
+  const revWords = wordsArr.map((word) => word.split('').reverse().join(''));
+  return revWords.join(' ');
 }
 
 /**
