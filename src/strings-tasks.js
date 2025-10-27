a886aab935f2bf856028d16f16abb7f286711d1f
dzzuze <notinsidik@gmail.com>
2025-10-27 13:52:37 +0300

    feat: implement countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -330,8 +330,15 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  let strSum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (vowels.includes(str[i])) {
+      strSum += 1;
+    }
+  }
+  return strSum;
 }
 
 /**
